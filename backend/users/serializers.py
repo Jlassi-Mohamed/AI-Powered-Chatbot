91b821b2c0703eceb961d36ed8cc8dd7d3b6c115
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2025-10-04 21:14:48 +0100

    Add user authentication serializers
    - Implement UserSerializer for profile data
    - Add UserRegistrationSerializer with password handling
    - Include language preference field

---
+++ backend/users/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "first_name", "last_name"]
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'password', 'language_preference']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            password=validated_data['password']
+        )
+        return user
